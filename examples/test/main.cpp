eed9f465e6ecbb27e4898c2b0e15e28c8f357fc1
wangyonglin <admin@wangyonglin.com>
2024-09-28 17:13:08 +0800

    ok

--- examples/test/main.cpp
+++ examples/test/main.cpp
@@ -1,15 +1,14 @@
 #include <QCoreApplication>
-
 #include <QApplication>
-
 #include <Qt6FFmpeg/FFmpegPlayer>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-
     Qt6FFmpeg::FFmpegPlayer player;
     player.resize(800,600);
+   // player.play("https://media.w3.org/2010/05/sintel/trailer.mp4");
+    //player.play("http://vjs.zencdn.net/v/oceans.mp4");
     player.play("https://cesium.com/public/SandcastleSampleData/big-buck-bunny_trailer.mp4");
     a.connect(&player,&Qt6FFmpeg::FFmpegPlayer::finished,[&](){
     player.stop();
